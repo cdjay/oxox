5b228211157480668e0d832e5820ae442e5c0fb2
cdjay <76025226@qq.com>
2017-11-21 01:49:45 +0800

    vol 1.0

--- ox.py
+++ ox.py
@@ -1,6 +1,14 @@
-# ver 1.0
+# vol. 1.0
 
 # 1-生成所有的组合到数据库
 # 2-判断所有的组合分值
 # 3-计算拿到13张牌，各类组合几率是
-import MySQLdb
+
+poke='1384'
+poke=sorted(poke)
+print(poke)
+poke=poke+['1']
+poke=sorted(poke)
+print(poke)
+card=poke[0]+poke[1]+poke[2]+poke[3]+poke[4]
+print(card)
